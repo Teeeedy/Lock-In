5af67f2af53feefbadff70f33bb208d064cbf901
Teeeedy <eddyyan95@gmail.com>
2025-04-25 17:26:01 +1000

    Setting up connection with postgresql server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import pool from "./.config/db";
+
+console.log("hello");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/", async (req: Request, res: Response) => {
-  res.json({ message: "hello" });
+  const result = await pool.query("SELECT current_database()");
+  res.send(`The database name is ${result.rows[0].current_database}`);
 });
 
 app.listen("3000", () => {
